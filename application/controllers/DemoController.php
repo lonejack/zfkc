a350fc88ab64c06ad13462247e10f8db282ee671
Claudio Eterno <eterno.claudio@gmail.com>
2012-04-06 15:39:22 +0200

    20120406

--- application/controllers/DemoController.php
+++ application/controllers/DemoController.php
@@ -7,7 +7,50 @@ class DemoController extends Zend_Controller_Action
 	}
 	
 	public function standaloneAction(){
+		$form = new Application_Form_Standalone();
+		$form->setAction('/demo/standalone')
+		->setMethod('post');
+		$this->view->form =$form;
+		$this->view->headLink()->appendStylesheet('/styles/zend_form.css', $attributes = array());
 		
+		$sa_config = new Zend_Session_Namespace('sa_config');
+		if( !isset($sa_config->language) ) {
+			$locale=new Zend_Locale();
+			$sa_config->language = $locale->getLanguage();
+		}
+		if( !isset($sa_config->style) ) {
+			$sa_config->style = 'oxygen';
+		}
+		if( !isset($sa_config->directory) ) {
+			$sa_config->directory = 'images';
+		}
+		
+		$request = $this->getRequest();
+		
+		
+		if($request->isPost()) {
+			try {
+				if (! $form->ifValidRemoveHash ( $request->getPost () ))
+					throw new Exception ( 'Parametri non corretti' );
+				
+				$sa_config->language = $form->getValue('language');
+				$sa_config->style = $form->getValue('style');
+				$sa_config->directory = $form->getValue('directory');
+				
+				
+			} catch ( Exception $e ) {
+				/* rethrow it */
+				$this->view->error = $e->getMessage ();
+			}
+			$this->_redirect('/demo/standalone');
+		}
+		else {
+			$form->populate(
+				array(	'language'	=>	$sa_config->language, 
+						'style'		=>	$sa_config->style,
+						'directory'	=>	$sa_config->directory ));
+			
+		}
 	}
 
 	public function indexAction(){
---
+++ application/forms/Standalone.php
@@ -0,0 +1,96 @@
+<?php
+
+class Application_Form_Standalone extends Zend_Form
+{
+	const ANTI_CRSF_KEY = 'anti_csrf_standalone';
+	
+	protected $_default_area;
+	
+	public function __construct( $default_area = IMAGES_PATH)
+	{
+		$this->_default_area = $default_area;
+		
+		parent::__construct();
+	}
+	
+    public function removeAntiCsrf()
+    {
+    	$this->removeElement(self::ANTI_CRSF_KEY);
+    	return ;
+    }
+    
+	public function ifValidRemoveHash($data)
+    {
+    	$ret = $this->isValid($data);
+    	if($ret)
+    		$this->removeAntiCsrf();
+    	return $ret;
+    }
+    
+    public function init()
+    {
+        /* Form Elements & Other Definitions Here ... */
+    	$this->setElementsBelongTo('standalone');
+        $language = new Zend_Form_Element_Select('language', array(
+            'label'    => 'Language: ',
+            'required' => true,
+        	'onChange' => 'submit()'
+        ));
+        $language->addMultiOptions(array(
+        		"af"	  =>   "Afrikaans",
+        		"pt_BR"   =>   "Brazilian Portuguese",
+        		"bg"	  =>   "Bulgarian",
+        		"ca"	  =>   "Catalan",
+        		"zh_CN"   =>   "Chinese Simplified",
+        		"cs"	  =>   "Czech",
+        		"da"	  =>   "Danish",
+        		"nl"	  =>   "Dutch",
+        		"en"	  =>   "English",
+        		"fi"	  =>   "Finnish",
+        		"fr"	  =>   "French",
+        		"de"	  =>   "German",
+        		"el"	  =>   "Greek",
+        		"hu"	  =>   "Hungarian",
+        		"it"	  =>   "Italian",
+        		"ja"	  =>   "Japanese",
+        		"lt"	  =>   "Lithuanian",
+        		"no"	  =>   "Norwegian",
+        		"fa"	  =>   "Persian",
+        		"pl"	  =>   "Polish",
+        		"pt"	  =>   "Portuguese",
+        		"ro"	  =>   "Romainan",
+        		"ru"	  =>   "Russian",
+        		"sk"	  =>   "Slovak",
+        		"es"	  =>   "Spanish",
+        		"sv"	  =>   "Swedish",
+        		"tr"	  =>   "Turkish",
+        		"uk"	  =>   "Ukrainian",
+        		"vi"	  =>   "Vietnamese"));
+        $this->addElement($language);
+        $style = new Zend_Form_Element_Select('style', array(
+        		'label'    => 'Style: ',
+        		'required' => true,
+        		'onChange' => 'submit()'
+        ));
+        $style->addMultiOptions(array(
+        		"dark"	  		=>   "dark",
+        		"oxigen"	  =>   "oxigen"));
+        $this->addElement($style);
+        
+        $directory = new Zend_Form_Element_Select('directory', array(
+        		'label'    => 'Directory: ',
+        		'required' => true,
+        		'onChange' => 'submit()'
+        ));
+        $directory->addMultiOptions(array(
+        		"images"	=>   "images",
+        		"flash"		=>   "flash",
+        		"files"	  	=>   "files"));
+        $this->addElement($directory);
+        
+    	$this->addElement('hash', self::ANTI_CRSF_KEY, array('salt' => 'unique'));
+    	$this->addElement('hidden', 'id');
+        $this->setAttrib('accept-charset', 'utf-8');
+    }
+}
+
